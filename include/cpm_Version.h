eec087d266e15865fc4f85b4a7f582b3a4307ce4
Kenji Ono <keno@riken.jp>
2013-06-29 11:34:22 +0900

    r11 update to 1.0.12

--- include/cpm_Version.h
+++ include/cpm_Version.h
@@ -9,18 +9,16 @@
 /**
  * @file   cpm_Version.h
  * CPMバージョン情報のヘッダーファイル
- * @author University of Tokyo
- * @date   2012/05/31
  */
 
 #ifndef _CPM_VERSION_H_
 #define _CPM_VERSION_H_
 
 /** CPMライブラリのバージョン */
-#define CPM_VERSION_NO  "1.0.11"
+#define CPM_VERSION_NO  "1.0.12"
 
 /** CPMライブラリのリビジョン */
-#define CPM_REVISION    "20130502_1200"
+#define CPM_REVISION    "20130629_1200"
 
 #endif /* _CPM_VERSION_H_ */
 
