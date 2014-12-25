ce89c5a3df2d2bddc3bfc13ae07282f76fc81582
Kenji Ono <keno@riken.jp>
2014-12-25 12:23:59 +0900

    r28 update user guide

--- include/cpm_Version.h
+++ include/cpm_Version.h
@@ -18,10 +18,10 @@
 #define _CPM_VERSION_H_
 
 /** CPMライブラリのバージョン */
-#define CPM_VERSION_NO  "1.2.2"
+#define CPM_VERSION_NO  "1.2.3"
 
 /** CPMライブラリのリビジョン */
-#define CPM_REVISION    "20140519_2100"
+#define CPM_REVISION    "20141225_1300"
 
 #endif /* _CPM_VERSION_H_ */
 
